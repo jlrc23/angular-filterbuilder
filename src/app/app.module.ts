9f36c446a2c65c76c9647bde2c83944716e28d6c
jlrc23 <jlrc23@gmail.com>
2021-05-12 16:13:23 -0500

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FilterBuilderModule } from './filter-builder/filter-builder.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, FilterBuilderModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
--- src/app/filter-builder/filter-builder.module.ts
+++ src/app/filter-builder/filter-builder.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FilterBuilderComponent } from './filter-builder/filter-builder.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule],
+  declarations: [FilterBuilderComponent],
+  exports: [FilterBuilderComponent]
 })
-export class FilterBuilderModule { }
+export class FilterBuilderModule {}
