edeb7189e08e5f2b719a14c83c2c7dfeea77225a
jlrc23 <jlrc23@gmail.com>
2021-05-12 13:55:50 -0500

    Add filter-builder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FilterBuilderComponent } from './filter-builder/filter-builder.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, FilterBuilderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/filter-builder/filter-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-builder',
+  templateUrl: './filter-builder.component.html',
+  styleUrls: ['./filter-builder.component.css']
+})
+export class FilterBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
